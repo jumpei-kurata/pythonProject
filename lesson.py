51b5b620d32c1130d636f40144be61325a099fdf
jumpei-kurata <se.jumpei114kurata@gmail.com>
2023-01-20 10:44:29 +0900

    Changes

---
+++ lesson.py
@@ -0,0 +1,11 @@
+import datetime
+
+now = datetime.datetime.now()
+print(now)
+print(now.isoformat())
+print(now.strftime('%d/%m/%y-%H%M%S%f'))
+
+today = datetime.date.today()
+print(today)
+print(today.isoformat())
+print(today.strftime('%d/%m/%y'))
