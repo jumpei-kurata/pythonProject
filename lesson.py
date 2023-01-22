d2946e282e8324cbdcb56a55600a8646c5974c75
jumpei-kurata <se.jumpei114kurata@gmail.com>
2023-01-22 16:29:42 +0900

    Changes

--- lesson.py
+++ lesson.py
@@ -1,11 +1,36 @@
-import datetime
+import sqlite3
 
-now = datetime.datetime.now()
-print(now)
-print(now.isoformat())
-print(now.strftime('%d/%m/%y-%H%M%S%f'))
+import conda_build.post
 
-today = datetime.date.today()
-print(today)
-print(today.isoformat())
-print(today.strftime('%d/%m/%y'))
+# conn = sqlite3.connect('test_sqlite.db')
+conn = sqlite3.connect(':memory:')
+
+curs = conn.cursor()
+
+curs.execute(
+    'CREATE TABLE persons(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)')
+conn.commit()
+
+curs.execute(
+    'INSERT INTO persons(name) values("Mike")'
+)
+conn.commit()
+
+curs.execute('SELECT * FROM persons')
+print(curs.fetchall())
+
+curs.execute(
+    'INSERT INTO persons(name) values("Nancy")')
+curs.execute(
+     'INSERT INTO persons(name) values("Jun")')
+conn.commit()
+
+curs.execute('UPDATE persons set name= "MICHEL" WHERE name = "Mike"')
+conn.commit()
+
+curs.execute('DELETE FROM persons WHERE name = "MICHEL"')
+conn.commit()
+
+curs.execute('SELECT * FROM persons')
+print(curs.fetchall())
+conn.close()
--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-# This is a sample Python script.
+import yaml
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+with open('config.yml', 'w') as yaml_file:
+    yaml.dump({
+        'web_server': {
+            'host': '127.0.0.1',
+            'port': 80
+        },
+        'db_server': {
+            'host': '127.0.0.1',
+            'port': 3306
+        }
+    }, yaml_file, default_flow_style=False)
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open('config.yml', 'r') as yaml_file:
+    data = yaml.load(yaml_file, Loader=yaml.Loader)
+    print(data, type(data))
+    print(data['web_server']['host'])
+    print(data['web_server']['port'])
+    print(data['db_server']['host'])
+    print(data['db_server']['port'])
