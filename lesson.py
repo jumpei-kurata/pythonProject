752a8b01ac3e1dfd787a6b2e51d6cc24741b5252
jumpei-kurata <se.jumpei114kurata@gmail.com>
2023-01-23 16:19:01 +0900

    tox

--- lesson.py
+++ lesson.py
@@ -1,12 +1,37 @@
-from bs4 import BeautifulSoup
-import requests
+# from bs4 import BeautifulSoup
+# import requests
+#
+# html = requests.get('https://www.python.org')
+# # print(html.text)
+# soup = BeautifulSoup(html.text, 'lxml')
+#
+# titles = soup.find_all('title')
+# print(titles)
+#
+# intro = soup.find_all('div', {'class': 'introduction'})
+# print(intro[0].text)
+#
+#
+# import unittest
+#
+# # import calculation
+#
+#
+# class Cal(object):
+#     def add_num_and_double(self, x, y):
+#         """Add and double
+#
+#         >>> c = Cal()
+#         >>> c.add_num_and_double(1, 1)
+#         4
+#
+#         """
+#         result = x + y
+#         result *= 2
+#         return result
+#
+# if __name__ == '__main__':
+#     import doctest
+#     doctest.testmod()
 
-html = requests.get('https://www.python.org')
-# print(html.text)
-soup = BeautifulSoup(html.text, 'lxml')
 
-titles = soup.find_all('title')
-print(titles)
-
-intro = soup.find_all('div', {'class': 'introduction'})
-print(intro[0].text)
