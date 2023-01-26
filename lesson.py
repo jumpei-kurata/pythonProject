2f91ba4f0aef6d499aa7560dc5eabd0b680aef02
jumpei-kurata <se.jumpei114kurata@gmail.com>
2023-01-26 23:09:25 +0900

    numpy pandas matplotlib scikit

--- lesson.py
+++ lesson.py
@@ -1,16 +1,2 @@
-import hashlib
-
-print(hashlib.sha256(b'test').hexdigest())
-print(hashlib.sha256(b'test').hexdigest())
-
-user_name = 'user1'
-user_pass = 'password'
-db = {}
-
-def get_digest(password):
-    password = bytes(user_pass, 'utf-8')
-    digest = hashlib.sha256(password).hexdigest()
-    db[user_name] = get_digest(user_pass)
-
-print(db)
+import pandas
 
