169b9d83c595b2f05d8e91e66c5fe6adda8211bc
jumpei-kurata <se.jumpei114kurata@gmail.com>
2023-01-27 17:45:29 +0900

    Async コルーチン

--- lesson.py
+++ lesson.py
@@ -1,14 +1,24 @@
+import asyncio
+import multiprocessing
+import threading
+import time
+
+loop = asyncio.get_event_loop()
 
 
+# @asyncio.coroutine
+async def worker():
+    print('start')
+    # yield from asyncio.sleep(2)
+    await asyncio.sleep(2)
+    print('stop')
 # def g_hello():
 #     yield 'hello 1'
 #     yield 'hello 2'
 #     yield 'hello 3'
 
-def g_hello():
-    r = yield from 'hello'
-    yield r
+if __name__ == '__main__':
+    loop.run_until_complete(asyncio.wait([worker(), worker()]))
+    loop.close()
 
-g = g_hello()
-print(next(g))
-print(next(g))
+
