9b468d456c6a8eb001a3f1ac4a1c2ebb2eeff51a
jumpei-kurata <se.jumpei114kurata@gmail.com>
2023-01-27 17:34:24 +0900

    Async

--- lesson.py
+++ lesson.py
@@ -1,2 +1,14 @@
-import pandas
 
+
+# def g_hello():
+#     yield 'hello 1'
+#     yield 'hello 2'
+#     yield 'hello 3'
+
+def g_hello():
+    r = yield  'hello'
+    yield r
+
+g = g_hello()
+print(next(g))
+print(next(g))
