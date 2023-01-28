10c1031b91ae210caaa629618fd5677f452dffd6
jumpei-kurata <se.jumpei114kurata@gmail.com>
2023-01-28 11:45:31 +0900

    Async

--- lesson.py
+++ lesson.py
@@ -1,24 +1,35 @@
 import asyncio
-import multiprocessing
-import threading
-import time
 
 loop = asyncio.get_event_loop()
 
 
-# @asyncio.coroutine
-async def worker():
-    print('start')
-    # yield from asyncio.sleep(2)
-    await asyncio.sleep(2)
-    print('stop')
-# def g_hello():
-#     yield 'hello 1'
-#     yield 'hello 2'
-#     yield 'hello 3'
+async def worker1(condition):
+    async with condition:
+        await condition.wait()
+        print('asyncio native start1')
+        await asyncio.sleep(3)
+        print('asyncio native stop1')
+
+
+async def worker2(condition):
+    async with condition:
+        await condition.wait()
+        print('asyncio native start2')
+        await asyncio.sleep(3)
+        print('asyncio native stop2')
+
+
+async def worker3(condition):
+    await asyncio.sleep(1)
+    async with condition:
+        print('asyncio native start3')
+        await asyncio.sleep(3)
+        print('asyncio native stop3')
+        condition.notify_all()
+
 
 if __name__ == '__main__':
-    loop.run_until_complete(asyncio.wait([worker(), worker()]))
+    condition = asyncio.Condition()
+    loop.run_until_complete(asyncio.wait(
+        [worker1(condition), worker2(condition), worker3(condition)]))
     loop.close()
-
-
